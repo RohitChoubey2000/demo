0ab40c5ce39e7eac85182ae6dd0b2a1c9f763615
RohitChoubey <rohitranjan488@gmail.com>
2025-10-30 22:43:58 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+
+ const express = require("express");//import
+ const db = require("./db")
+ const jwt = require("jsonwebtoken")
+ const multer = require("multer");
+ const bcrypt = require("bcrypt");
+const path = require("path");
+ const app = express(); // object
+
+
+
+
+
+
+ app.get("/api/users", (request, response) => {
+   db.query("SELECT * FROM users", (error, results) => {
+     if (error) {
+       console.error("Database error:", error);
+      return response.status(500).json({ message: "Server internal error" });
+     }
+    response.status(200).json(results); // return database
+  });
+ });
+
+
+
+
+
+
+
+
+ app.listen(3500, () => {
+  console.log("Server is running on 3500");
+})
